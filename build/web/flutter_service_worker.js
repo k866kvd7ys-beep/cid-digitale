b5911dfd6f32738e369ac63d1b53f616a57481e6
Antonio Privitera <antonioprivitera@MacBook-Air-di-Antonio.local>
2026-05-04 21:13:59 +0200

    build web email details

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a4256abaea87e7c9c8ffaab466198aae",
+const RESOURCES = {"flutter_bootstrap.js": "763db3aac32d25eda2158e77b8729a12",
 "version.json": "888943b38089b79929d4fe1fadd39b0b",
 "index.html": "1b479eb7cd2b630529656f895428de9b",
 "/": "1b479eb7cd2b630529656f895428de9b",
-"main.dart.js": "af533e6f05d9e67662b9ed147d44c869",
+"main.dart.js": "e01689bd57025e07ba179cc4417d116e",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
